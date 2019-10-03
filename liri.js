fab4e9098fddfd56797bbe99691ce34e6f930d7a
Tom Cox <thomasxcox731@gmail.com>
2019-10-03 16:47:55 -0500

    Rotten Tomatoes JSON error fixed.

--- liri.js
+++ liri.js
@@ -64,7 +64,7 @@ function movieThis() {
             console.log("Movie title: " + movie.Title + "\n",
                 "Release Year: " + movie.Year + "\n",
                 "IMDB Rating: " + movie.imdbRating + "\n",
-                // "Rotten Tomatoes Rating: " + movie.Ratings[1].value + "\n", 
+                "Rotten Tomatoes Rating: " + movie.Ratings[1].Value + "\n", 
                 "Country: " + movie.Country + "\n",
                 "Language: " + movie.Language + "\n",
                 "Plot: " + movie.Plot + "\n",
@@ -77,17 +77,11 @@ function movieThis() {
 //Using fs to "Do what it says"//
 function doThis() {
     fs.readFile("random.txt", "utf-8", function (error, data) {
-
-        // If the code experiences any errors it will log the error to the console.
         if (error) {
             return console.log(error);
         }
-        // We will then print the contents of data
         console.log(data);
-        // Then split it by commas (to make it more readable)
         var dataArr = data.split(",");
-        // We will then re-display the content as an array for later use.
-        // console.log(dataArr);
         process.argv[2]=dataArr[0];
         input = dataArr[1];
         spotifyThis(input);
